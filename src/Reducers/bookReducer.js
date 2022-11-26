cc337f43b2128d505bb994a84e020a4af21248e2
dargonshean <dragonshean@gmail.com>
2022-11-26 22:02:27 +0800

    add comment text of useReducer

--- src/Reducers/bookReducer.js
+++ src/Reducers/bookReducer.js
@@ -1,5 +1,7 @@
 import { v1 } from 'uuid';
 
+//當有多個需要改變state的方式   使用useReducer 來統整各個執行改變的項目
+
 //action 只能透過 dispatch 來調用，dispatch 會將 action 傳給 reducer。
 
 export const bookReducer = (state, action) => { //state = books, action = 更新state的方式
