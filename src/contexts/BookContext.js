ede3e7acd8295d558364c75586be7b0cc005403f
dargonshean <dragonshean@gmail.com>
2022-11-26 22:26:58 +0800

    add localStorage, stop data removed when reRender.

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,4 +1,4 @@
-import { createContext, /*useState*/ useReducer } from 'react'
+import { createContext, /*useState*/ useReducer, useEffect } from 'react'
 // import { v1 } from 'uuid';
 import NewBookForm from '../components/NewBookForm';
 import { bookReducer } from '../Reducers/bookReducer';
@@ -12,7 +12,14 @@ const BookContextProvider = (props) => {
         // {title: "King Rises", id: 2 },
         // {title: 'King Fall again', id: 3 },
         // {title: 'King is stupid', id: 4 }
-    ]); 
+    ], () => {//使用useReducer 第三個參數 initialAction 使之回傳原本的books陣列
+        const localData = localStorage.getItem('books'); //從localStorage 取回存取過的books/JSON
+        return localData ? JSON.parse(localData) : []   //只取回localData, 沒有則回傳空陣列
+    }); 
+    
+    useEffect(() => {//(只要reRender 就會執行)只要books有被改變, 就執行將books 轉換成JSON存到LocalStorage內
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books])
     
     return ( 
         <BookContext.Provider value = {{books, dispatch}}>
